305786297fe15bb79c1a638786be62b485dabff3
JafRojasCR <jafetalonsorb08@gmail.com>
2026-05-27 22:08:55 -0600

    Fixed small UI element in Class cancelling part

--- client/src/components/ClassCalendarPanel.jsx
+++ client/src/components/ClassCalendarPanel.jsx
@@ -1555,7 +1555,7 @@ const ClassCalendarPanel = ({
                           onMouseLeave={() => { if (!cancelInputFocused && !cancelReason) setShowCancelInput(false); }}
                         >
                           <div
-                            className="absolute left-1/2 -translate-x-1/2 -top-20 w-[240px] z-30 pointer-events-auto"
+                            className="absolute left-1/2 -translate-x-1/2 -top-20 w-[200px] z-30 pointer-events-auto"
                             style={{
                               transition: 'all 220ms cubic-bezier(0.2,0.9,0.2,1)',
                               transform: showCancelInput || cancelReason || cancelInputFocused ? 'translateX(-50%) translateY(0) scale(1)' : 'translateX(-50%) translateY(-6px) scale(0.96)',
